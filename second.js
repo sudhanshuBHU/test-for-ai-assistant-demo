227b9e309c7b663279405de3955a30a1f412fabe
Sudhanshu <sudhanshu007@bhu.ac.in>
2025-08-19 16:27:28 +0530

    add second js file

---
+++ second.js
@@ -0,0 +1,10 @@
+
+
+const second = () => {
+    console.log('second component loaded');
+    return (
+        <div>This is a testing for the bot "AI-Code-Review-Assistant"</div>
+    )
+}
+
+export default second;
